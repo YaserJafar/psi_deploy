38879b5c1f39e3cea96f62e4dc5d427f1fd23c30
YaserJafar <92629580+YaserJafar@users.noreply.github.com>
2024-03-14 23:26:21 +0100

    Update settings.py

--- persona/persona/settings.py
+++ persona/persona/settings.py
@@ -145,7 +145,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = TRUE
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:5173',
     'http://tutorialvue-q5t2.onrender.com',
